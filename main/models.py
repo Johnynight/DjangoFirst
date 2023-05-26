e0707e36bdcb213c293af34862107a29555b1327
vchurikov <teqilkka@gmail.com>
2023-05-26 19:02:47 +0600

    26.05.23 job work

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128,verbose_name='Заголовок')
+    body = models.TextField(verbose_name='Статься')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.title
