2c775d9764965e13c32fe89f2ab12ade0ed24f00
Slava <teqilkka@gmail.com>
2023-05-28 19:10:06 +0600

    Home commit

--- main/models.py
+++ main/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth import get_user_model
 import datetime
 
 User = get_user_model()
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=128,verbose_name='Заголовок')
-    body = models.TextField(verbose_name='Статься')
+    title = models.CharField(max_length=128, verbose_name='Заголовок')
+    body = models.TextField(verbose_name='Статья')
     draft = models.BooleanField(default=True)
-    changed_at = models.DateTimeField(auto_now=True,)
+    changed_at = models.DateTimeField(auto_now=True, )
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True, verbose_name='Автор')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+
     class Meta:
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
