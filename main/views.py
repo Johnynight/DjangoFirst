3bf3beb5b163b2ddea028f1f1816fe0366e73602
Slava <teqilkka@gmail.com>
2023-05-30 19:13:26 +0600

    asd

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+
 from .models import Post
+
 
 
 def home(request):
     posts = Post.objects.all().order_by('created_at')
     return render(request, 'main/home.html', {'posts': posts})
 
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'main/post_detail.html', {'post': post})
 
 def second_view(request):
     return render(request, 'main/second.html')
