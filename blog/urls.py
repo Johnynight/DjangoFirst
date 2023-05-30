9c9abd814925e0f08e603a6ebf2ae28137dc68d7
vchurikov <teqilkka@gmail.com>
2023-05-30 17:08:20 +0600

    30.05.23 job work

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,),
     path('', include('main.urls')),
     path('blog/', include('main.urls')),
     #path('base/', include('main.urls')),
