a586776d19df93a936b68bb4f733fe6bc50f9b5f
vchurikov <teqilkka@gmail.com>
2023-06-01 15:53:48 +0600

    01.05.23 job work

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import home, second_view, base_view
+# from .views import home, second_view,  post_detail, post_create
+from .views import PostList, PostDetail, PostCreate, PostUpdate
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('second/', second_view, name='second'),
-    path('base/', base_view, name='base'),
+    path('post/<int:pk>/update/',PostUpdate.as_view(), name='post_update'),
+    path('post/create/',PostCreate.as_view(),name='post_create'),
+    path('', PostList.as_view(), name='home'),
+    # path('second/', , name='second'),
+    path('post/<int:pk>/',PostDetail.as_view(),  name='post_detail'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.views.generic import DetailView, CreateView, ListView, UpdateView
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.models import User
 
 
+# def home(request):
+#     posts = Post.objects.all().order_by('created_at')
+#     return render(request, 'main/home.html', {'posts': posts})
+#
+#
+# def post_detail(request, id):
+#     try:
+#         post = Post.objects.get(id=id)
+#     except Post.DoesNotExist:
+#         pass
+#     return render(request, 'main/post_detail.html', {'post': post})
+#
+#
+# def second_view(request):
+#     return render(request, 'main/second.html')
+#
+#
+# def base_view(request):
+#     return render(request, 'main/base.html')
+#
+#
+# def post_create(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             cd = form.save(commit=False)
+#             cd.author = request.user
+#             cd.save()
+#             return redirect(home)
+#     else:
+#         form = PostForm()
+#     return render(request, 'main/post_create.html', {'form': form})
 
-def home(request):
-    posts = Post.objects.all().order_by('created_at')
-    return render(request, 'main/home.html', {'posts': posts})
+class PostList(ListView):
+    model = Post
+    template_name = 'main/home.html'
+    context_object_name = 'posts'
+    ordering = '-created_at'
 
-def post_detail(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, 'main/post_detail.html', {'post': post})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = User.objects.get(id=1)
+        return context
+    # def get_queryset(self):
+    # return Post.objects.all().order_by("-created_at")
 
-def second_view(request):
-    return render(request, 'main/second.html')
+
+class PostDetail(DetailView):
+    model = Post
 
 
-def base_view(request):
-    return render(request, 'main/base.html')
+class PostCreate(CreateView):
+    form_class = PostForm
+    template_name = 'main/post_create.html'
+    success_url = '/blog/'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/'
