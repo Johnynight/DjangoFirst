eea2079b3fc66aa19fdc297da81f724e3e90a650
vchurikov <teqilkka@gmail.com>
2023-06-12 16:46:10 +0600

    12.06.23 job work

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import logout_view
 
+from .views import register
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', logout_view, name='logout')
-
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', logout_view, name='logout')
+    path('', include('django.contrib.auth.urls')),
+    path('register/',register, name='register')
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import UserRegistrationForm
 
 from django.contrib.auth import logout
 from django.shortcuts import redirect
@@ -8,4 +9,18 @@ def logout_view(request):
     logout(request)
     return redirect('home')  # Замените 'home' на URL-шаблон вашей домашней страницы
 
+
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
